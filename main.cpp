777bd420e4b7cc40a2ad781d8ad7b878931ee71a
sstrang15 <sstrang11@gatech.edu>
2021-05-28 02:43:47 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+// constructing vectors
+#include <iostream>
+#include <vector>
+#include <cstdio>
+
+
+struct Level {
+	vector<Node> members;
+	Node from_node;
+
+}
+
+struct Node {
+
+}
+
+next_level(Node n) {
+
+}
+
+previous_level(Node n) {
+
+}
+
+
+int main ()
+{
+	// make nodal map
+	// generate first level
+	// select choice
+	// move to next level
+	// option to go back level 
+
+}
